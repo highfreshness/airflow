54a4d273caf5fd60ec2b0ca4c12a53399fd1ec3c
highfreshness <meteo0718@gmail.com>
2025-04-06 20:55:09 +0900

    Fix: Run function

--- dags/dags_bash_operator.py
+++ dags/dags_bash_operator.py
@@ -4,10 +4,14 @@ from airflow.decorators import dag, task
 
 @dag(
     schedule="0 21 * * *",
-    start_date=pendulum.datetime(2025, 4, 6, tz="UTC"),
+    start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
     catchup=False,
 )
 def example_bash_dag():
     @task.bash
     def bash_example():
         return "echo 'Hello, World!'"
+    
+    bash_example()
+
+example_bash_dag()
